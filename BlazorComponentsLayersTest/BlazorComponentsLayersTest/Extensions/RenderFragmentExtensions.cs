ceecd83bcac4744085ecfddd54c5bca10c10266b
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-11 19:44:41 +0100

    Add improved tree layout

---
+++ BlazorComponentsLayersTest/BlazorComponentsLayersTest/Extensions/RenderFragmentExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Reflection;
+
+namespace BlazorComponentsLayersTest.Extensions
+{
+    public static class RenderFragmentExtensions
+    {
+        public static RenderFragment CreateComponent<T>(params object[] arguments)
+             where T : notnull, IComponent
+        {
+            var type = typeof(T);
+            var parameterProperties = type.GetProperties().Where(p => p.GetCustomAttribute(typeof(ParameterAttribute)) != null).ToArray();
+
+            return builder =>
+            {
+                builder.OpenComponent<T>(0);
+
+                for (int i = 0; i < arguments.Length; i++) 
+                    builder.AddAttribute(0, parameterProperties[i].Name, arguments[i]);
+
+                builder.CloseComponent();
+            };
+        }
+    }
+}
