c78a87f24aba83b2a2d97166efbac5395f05a556
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-26 00:09:46 +0100

    add right click property menu

---
+++ BlazorComponentsLayersTest/BlazorComponentsLayersTest/wwwroot/js/functions.js
@@ -0,0 +1,9 @@
+﻿function getBoundingClientRectByClass(className) {
+    const element = document.getElementsByClassName(className)[0];
+    return element.getBoundingClientRect();
+};
+
+function setPosition(element, x, y) {
+    element.style.left = x + "px";
+    element.style.top = y + "px";
+}
