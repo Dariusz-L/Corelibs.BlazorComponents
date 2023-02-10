8913ecec14ca7c66e65ae20183090a02da8e0c21
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-02-11 00:14:39 +0100

    Edit page etc

--- Corelibs.BlazorComponents/Corelibs.BlazorViews/wwwroot/js/functions.js
+++ Corelibs.BlazorComponents/Corelibs.BlazorViews/wwwroot/js/functions.js
@@ -1,4 +1,11 @@
-﻿function getBoundingClientRectByClass(className) {
+﻿function getWindowDimensions() {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+};
+
+function getBoundingClientRectByClass(className) {
     const element = document.getElementsByClassName(className)[0];
     return element.getBoundingClientRect();
 };
