d1b7e30e7a2ac77d04f05a9442c0f9fb62a53a3d
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-02-19 16:14:50 +0100

    Move common code to base class

---
+++ Corelibs.BlazorComponents/Corelibs.BlazorViews/Layouts/BaseComponent.cs
@@ -0,0 +1,37 @@
+﻿using Common.Basic.Blocks;
+using Corelibs.BlazorShared;
+using Mediator;
+using Microsoft.AspNetCore.Components;
+using Corelibs.Basic.Reflection;
+
+namespace Corelibs.BlazorViews.Layouts
+{
+    public abstract class BaseComponent<TQuery, TQueryOut, TVM> : ComponentBase
+        where TQuery : IQuery<Result<TQueryOut>>
+        where TVM : new()
+    {
+        [Inject] protected ICommandExecutor _commands { get; set; }
+        [Inject] protected IQueryExecutor _queries { get; set; }
+
+        protected TVM _vm { get; private set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await RefreshViewModel();
+        }
+
+        public async Task RefreshView()
+        {
+            await RefreshViewModel();
+            await InvokeAsync(StateHasChanged);
+        }
+
+        public async Task RefreshViewModel()
+        {
+            var result = await _queries.Execute<TQuery, TQueryOut>(QueryParameter);
+            _vm = result.GetPropertyValue<TVM>() ?? _vm;
+        }
+
+        protected abstract string QueryParameter { get; }
+    }
+}
