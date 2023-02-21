1f234a07a071ace202ba7070762ad976ded11adf
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-02-21 01:02:26 +0100

    Add view interface

--- Corelibs.BlazorComponents/Corelibs.BlazorViews/Layouts/BaseComponent.cs
+++ Corelibs.BlazorComponents/Corelibs.BlazorViews/Layouts/BaseComponent.cs
@@ -6,12 +6,16 @@ using Corelibs.Basic.Reflection;
 
 namespace Corelibs.BlazorViews.Layouts
 {
-    public abstract class BaseComponent<TQuery, TQueryOut, TVM> : ComponentBase
+    public abstract class BaseComponent<TQuery, TQueryOut, TVM, TView> : ComponentBase
         where TQuery : IQuery<Result<TQueryOut>>
         where TVM : new()
+        where TView : IView<TVM>
     {
         [Inject] protected ICommandExecutor _commands { get; set; }
         [Inject] protected IQueryExecutor _queries { get; set; }
+        [Inject] protected NavigationManager _navigation { get; set; }
+
+        protected TView _view;
 
         protected TVM _vm { get; private set; } = new();
 
@@ -30,6 +34,9 @@ namespace Corelibs.BlazorViews.Layouts
         {
             var result = await _queries.Execute<TQuery, TQueryOut>(QueryParameter);
             _vm = result.GetPropertyValue<TVM>() ?? _vm;
+
+            _view.Model = _vm;
+            await _view.RefreshViewModel();
         }
 
         protected abstract string QueryParameter { get; }
---
+++ Corelibs.BlazorComponents/Corelibs.BlazorViews/Layouts/IView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corelibs.BlazorViews.Layouts
+{
+    public interface IView<TVM>
+    {
+        TVM Model { get; set; }
+        Task RefreshView();
+        Task RefreshViewModel();
+    }
+}
